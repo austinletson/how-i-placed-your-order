da2b3e700e293fc55be7d9dba043e7425dfca866
Austin <AL185189@ncr.com>
2019-07-31 14:27:22 -0400

    Alarm Manager service running

--- HowIPlacedYourOrder/app/src/main/java/com/example/howiplacedyourorder/AlarmService.java
+++ HowIPlacedYourOrder/app/src/main/java/com/example/howiplacedyourorder/AlarmService.java
@@ -1,16 +1,37 @@
 package com.example.howiplacedyourorder;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
-public class AlarmService extends Service {
-    public AlarmService() {
+
+public class AlarmService extends Service
+{
+    Alarm alarm = new Alarm();
+    public void onCreate()
+    {
+        super.onCreate();
     }
 
     @Override
-    public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
+    public int onStartCommand(Intent intent, int flags, int startId)
+    {
+        alarm.setAlarm(this);
+        Log.d("Hackathon", "background service started");
+        return START_STICKY;
+    }
+
+    @Override
+    public void onStart(Intent intent, int startId)
+    {
+        alarm.setAlarm(this);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent)
+    {
         throw new UnsupportedOperationException("Not yet implemented");
     }
 }
